28d354431b8ec3f3a29ccda8a1ed2662c5a944ec
Daniel McEwen <daniel@cloudthing.com>
2020-01-07 17:13:58 +0000

    removed unused dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,10 @@
 module.exports = {
-    "setupTestFrameworkScriptFile": "<rootDir>/enzymeConfig.js",
-    "setupFiles": [
-      "<rootDir>/enzymeConfig.js"
-    ],
-    "snapshotSerializers": ["jest-emotion/serializer"],
-    "moduleNameMapper": {
-      "^.+\\.svg$": "<rootDir>/src/__mocks__/svgMock.js"},
-    "moduleFileExtensions": [
-      "js",
-      "jsx",
-      "json"
-    ],
-    "testPathIgnorePatterns": [
-      "/node_modules/",
-      "/lib/"
-    ],
-  }
+  setupFilesAfterEnv: ['<rootDir>/enzymeConfig.js'],
+  setupFiles: ['<rootDir>/enzymeConfig.js'],
+  snapshotSerializers: ['jest-emotion/serializer'],
+  moduleNameMapper: {
+    '^.+\\.svg$': '<rootDir>/src/__mocks__/svgMock.js'
+  },
+  moduleFileExtensions: ['js', 'jsx', 'json'],
+  testPathIgnorePatterns: ['/node_modules/', '/lib/']
+}
