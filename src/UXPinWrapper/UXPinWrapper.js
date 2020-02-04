efe2c4df03bea2eaed92cf8574458a56abd0b0c5
Daniel McEwen <daniel@cloudthing.com>
2020-02-04 12:52:16 +0000

    Re-added theme

--- src/UXPinWrapper/UXPinWrapper.js
+++ src/UXPinWrapper/UXPinWrapper.js
@@ -4,5 +4,5 @@ import { ThemeProvider } from 'emotion-theming'
 import theme from '@rebass/preset'
 
 export default function UXPinWrapper ({ children }) {
-  return <ThemeProvider>{children}</ThemeProvider>
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
 }
